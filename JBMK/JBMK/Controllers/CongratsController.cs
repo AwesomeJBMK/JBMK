5e6ce14f47d7cb5a4bd09b0e5cd3cfef1020c667
Keen Guo <keen.guo@greendotcorp.com>
2018-11-23 23:01:05 +0800

    integrated

---
+++ JBMK/JBMK/Controllers/CongratsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JBMK.Controllers
+{
+    public class CongratsController : Controller
+    {
+        // GET: Congrats
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
